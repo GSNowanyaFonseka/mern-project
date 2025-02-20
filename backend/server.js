73a5a1891d9d93a1fb3db883bdad5816e81571ec
Sandewdi Nowanya <nowanyafonseka@gmail.com>
2025-02-20 22:59:09 +0530

    chore: Make API and React app accessible on localhost:5000

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 // const express = require('express');  // tradition way
 import express from 'express'; // modern way (we need to add type = "module" in the package.json file to use import and export)
 import dotenv from "dotenv";
+import path from 'path';
 import { connectDB } from './config/db.js';
+
 import productRoutes from "./routes/product.route.js"
 // import Product from './models/product.model.js';
 // import mongoose from 'mongoose';
@@ -10,10 +12,21 @@ import productRoutes from "./routes/product.route.js"
 dotenv.config();
 
 const app = express(); // call the function 
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());  // allows us  to accept JSON data in the body (req.body) of the request
 
-app.use("/api/products",productRoutes)
+app.use("/api/products",productRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
 // listen for a port
 // local host 5000 - root rout (homepage)
